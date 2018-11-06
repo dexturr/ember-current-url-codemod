10b1629de9920e3323ad727c8f9274f2a6270a25
Dexter Edwards <dexter.edwards@ig.com>
2018-11-06 13:03:52 +0000

    Add basic codemod logic

---
+++ index.js
@@ -0,0 +1,38 @@
+// Press ctrl+space for code completion
+export default function transformer(file, api) {
+    const j = api.jscodeshift;
+  
+    return j(file.source)
+      .find(j.CallExpression, {
+        arguments: [{
+          type: 'CallExpression',
+          callee: {
+            type: 'Identifier',
+            name: 'currentURL',
+          },
+        }],
+        callee: {
+          object: {
+            type: 'Identifier',
+            name: 'assert',
+          },
+          property: {
+            type: 'Identifier',
+            name: 'equal',
+          },
+        },
+      })
+      .forEach((path) => {
+        const { value } = path;
+        const remainingArgs = value.arguments.slice(1);
+        const callExpression = j.callExpression(
+          j.memberExpression(
+            j.identifier('assert'),
+            j.identifier('currentUrl')
+          ),
+          remainingArgs
+        );
+        path.replace(callExpression);
+      })
+      .toSource();
+  }
